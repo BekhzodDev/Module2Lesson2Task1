5111b1226ccb4bfefc104e97528b43f3e0a5d719
IkramovBehzod <bekhzod.ikramov93@gmail.com>
2021-05-04 04:27:10 +0500

    Version22

--- src/main/java/task1/task1/AttachmentService.java
+++ src/main/java/task1/task1/AttachmentService.java
@@ -3,6 +3,7 @@ package task1.task1;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import task1.task1.entity.Attachment;
@@ -10,7 +11,11 @@ import task1.task1.entity.AttachmentContent;
 import task1.task1.repository.AttachmentContentRepository;
 import task1.task1.repository.AttachmentRepository;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AttachmentService {
@@ -36,4 +41,26 @@ public class AttachmentService {
         return new Result("File saqlandi", true, savedAttachment.getId());
 
     }
-}
+    public List<Attachment> getAttachments(){
+        return attachmentRepository.findAll();
+    }
+    public Attachment getAttachment(Integer id){
+        Optional<Attachment> optionalAttachment = attachmentRepository.findById(id);
+        return optionalAttachment.orElse(null);
+    }
+    public void downloadFile(Integer id, HttpServletResponse response) throws IOException {
+        Optional<Attachment> optionalAttachment = attachmentRepository.findById(id);
+        if (optionalAttachment.isPresent()) {
+            Attachment attachment = optionalAttachment.get();
+            Optional<AttachmentContent> optionalAttachmentContent = attachmentContentRepository.findByAttachmentId(id);
+            if (optionalAttachmentContent.isPresent()) {
+                AttachmentContent attachmentContent = optionalAttachmentContent.get();
+                response.setHeader("Content-Disposition", "attachment; filename=\"" + attachment.getName() + "\"");
+                response.setContentType(attachment.getContentType());
+                FileCopyUtils.copy(attachmentContent.getBytes(), response.getOutputStream());
+            }
+        }
+    }
+
+        }
+
--- src/main/java/task1/task1/repository/AttachmentContentRepository.java
+++ src/main/java/task1/task1/repository/AttachmentContentRepository.java
@@ -3,5 +3,9 @@ package task1.task1.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import task1.task1.entity.AttachmentContent;
 
+import java.util.Optional;
+
 public interface AttachmentContentRepository extends JpaRepository<AttachmentContent, Integer> {
+
+    Optional<AttachmentContent> findByAttachmentId(int attachment_id);
 }
